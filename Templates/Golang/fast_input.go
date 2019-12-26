fa014e829675afec4a0f9c3a05f232ad45c1e89a
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2019-12-27 00:59:40 +0900

    入力受け取りの高速バージョンのサンプル

---
+++ Templates/Golang/fast_input.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	x := nextInt()
+	fmt.Println(x)
+}
