0f32331103b9285c557ea26afbd643061931ac8e
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-01-07 00:08:05 +0900

    精進20200106

---
+++ PastProblems/DiffGray/ABC003A.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func nextFloat64() float64 {
+	f, _ := strconv.ParseFloat(next(), 64)
+	return f
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n := nextFloat64()
+	ans := 5000.0 * n * (n + 1) / n
+	fmt.Println(ans)
+}
