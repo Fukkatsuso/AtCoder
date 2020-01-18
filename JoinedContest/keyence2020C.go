5ceadd0c35486f2e9dd82a401a1f03f2b9a961cc
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-01-19 00:15:08 +0900

    keyence2020

---
+++ JoinedContest/keyence2020C.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n, k, s := nextInt(), nextInt(), nextInt()
+	a := make([]int, n)
+	for i := 0; i < k; i++ {
+		a[i] = s
+	}
+	for i := k; i < n; i++ {
+		if s > 1 {
+			a[i] = s - 1
+		} else {
+			a[i] = s + 1
+		}
+	}
+	for i := 0; i < n; i++ {
+		if i < n-1 {
+			putf("%d ", a[i])
+		} else {
+			putf("%d\n", a[i])
+		}
+	}
+	wt.Flush()
+}
+
+var (
+	sc  = bufio.NewScanner(os.Stdin)
+	rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+	wt  = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextFloat64() float64 {
+	f, _ := strconv.ParseFloat(next(), 64)
+	return f
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func nextFloat64s(n int) []float64 {
+	slice := make([]float64, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextFloat64()
+	}
+	return slice
+}
+
+func nextMega() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		l, p, _ := rdr.ReadLine()
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func putf(format string, a ...interface{}) {
+	fmt.Fprintf(wt, format, a...)
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
