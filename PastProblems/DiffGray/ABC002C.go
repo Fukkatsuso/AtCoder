20b3db63815385b615e3e5ee0964a3d99091ad08
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-01-21 00:33:09 +0900

    精進20200120

---
+++ PastProblems/DiffGray/ABC002C.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	x := nextInts(6)
+	x[2], x[4] = x[2]-x[0], x[4]-x[0]
+	x[3], x[5] = x[3]-x[1], x[5]-x[1]
+	s := float64(abs(x[2]*x[5]-x[3]*x[4])) / 2.0
+	puts(s)
+	wt.Flush()
+}
+
+var (
+	sc  = bufio.NewScanner(os.Stdin)
+	rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+	wt  = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextFloat64() float64 {
+	f, _ := strconv.ParseFloat(next(), 64)
+	return f
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func nextFloat64s(n int) []float64 {
+	slice := make([]float64, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextFloat64()
+	}
+	return slice
+}
+
+func nextMega() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		l, p, _ := rdr.ReadLine()
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func putf(format string, a ...interface{}) {
+	fmt.Fprintf(wt, format, a...)
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
---
+++ PastProblems/DiffGray/ABC015B.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n := nextInt()
+	cnt := 0
+	sum := 0
+	for i := 0; i < n; i++ {
+		a := nextInt()
+		if a > 0 {
+			cnt++
+			sum += a
+		}
+	}
+	puts(int(math.Ceil(float64(sum) / float64(cnt))))
+	wt.Flush()
+}
+
+var (
+	sc  = bufio.NewScanner(os.Stdin)
+	rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+	wt  = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextFloat64() float64 {
+	f, _ := strconv.ParseFloat(next(), 64)
+	return f
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func nextFloat64s(n int) []float64 {
+	slice := make([]float64, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextFloat64()
+	}
+	return slice
+}
+
+func nextMega() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		l, p, _ := rdr.ReadLine()
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func putf(format string, a ...interface{}) {
+	fmt.Fprintf(wt, format, a...)
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
