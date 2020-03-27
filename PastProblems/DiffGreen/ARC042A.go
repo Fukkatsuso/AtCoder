dd90ea438521483a0952a9fdfa9eaf0c72a571ac
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-03-27 22:52:59 +0900

    精進20200327

---
+++ PastProblems/DiffGreen/ARC042A.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, m := nextInt(), nextInt()
+	a := nextInts(m)
+
+	writed := make([]bool, n)
+	for i := m - 1; i >= 0; i-- {
+		if !writed[a[i]-1] {
+			puts(a[i])
+			writed[a[i]-1] = true
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if !writed[i] {
+			puts(i + 1)
+		}
+	}
+}
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
