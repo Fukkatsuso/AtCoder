360cced3f77af3deccad76185dc5bc417d8bd13d
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-05-02 00:14:02 +0900

    精進20200501

---
+++ PastProblems/DiffGreen/ARC091D.go
@@ -0,0 +1,64 @@
+// 解説AC
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, k := nextInt(), nextInt()
+
+	ans := 0
+	for b := 1; b <= n; b++ {
+		p, r := n/b, n%b
+		x := p*max(0, b-k) + max(0, r-k+1)
+		ans += x
+	}
+	if k == 0 {
+		ans -= n
+	}
+
+	puts(ans)
+}
