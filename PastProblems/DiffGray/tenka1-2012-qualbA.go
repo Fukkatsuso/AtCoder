273079e7c9014bc2ae1c531f56485fd4d22212bd
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-05-18 23:32:01 +0900

    精進20200518

---
+++ PastProblems/DiffGray/tenka1-2012-qualbA.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInt3() (int, int, int) {
+	return nextInt(), nextInt(), nextInt()
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	a, b, c := nextInt3()
+
+	for i := 1; i <= 127; i++ {
+		if i%3 == a && i%5 == b && i%7 == c {
+			puts(i)
+		}
+	}
+}
