b69c62da2608f4c6aec53bd631442d394c09119c
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-07-11 23:10:46 +0900

    aising2020

---
+++ JoinedContest/aising2020A.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	l, r, d := nextInt(), nextInt(), nextInt()
+	ans := r/d - (l-1)/d
+	puts(ans)
+}
