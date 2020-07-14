187a7ffcb7cbec09976671a6c38e0731e016eb6b
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-07-14 22:53:55 +0900

    精進20200714

---
+++ PastProblems/DiffLightBlue/ARC011C.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func diff(s, t string) int {
+	res := 0
+	for i := range s {
+		if s[i] != t[i] {
+			res++
+		}
+	}
+	return res
+}
+
+type Edge struct {
+	to, cost int
+}
+
+// 辺のリストで表現されるグラフ
+type Graph struct {
+	Edges [][]Edge // Edge[i][j]: 頂点iのj番目の辺
+	Pred  []int    // 遷移元
+	Cost  []int    // 始点からの最小コスト
+}
+
+// 頂点数nのリスト型グラフを返す
+func NewGraph(n int) *Graph {
+	g := &Graph{
+		Edges: make([][]Edge, n),
+		Pred:  make([]int, n),
+		Cost:  make([]int, n),
+	}
+	for i := 0; i < n; i++ {
+		g.Edges[i] = make([]Edge, 0)
+		g.Pred[i] = -1
+		g.Cost[i] = 1 << 60
+	}
+	return g
+}
+
+// 辺の追加
+func (g *Graph) AddEdge(from, to, cost int) {
+	g.Edges[from] = append(g.Edges[from], Edge{to, cost})
+}
+
+type Elem struct {
+	value    interface{}
+	priority int
+	index    int
+}
+
+type PriorityQueue []*Elem
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index, pq[j].index = i, j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	elem := x.(*Elem)
+	elem.index = n
+	*pq = append(*pq, elem)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(*pq)
+	elem := old[n-1]
+	old[n-1] = nil
+	elem.index = -1
+	*pq = old[0 : n-1]
+	return elem
+}
+
+// 0-indexed
+func (g *Graph) DijkstraSearch(origin int) {
+	// init
+	g.Cost[origin] = 0
+	done := make([]bool, len(g.Edges))
+	pq := make(PriorityQueue, 0)
+	heap.Init(&pq)
+	heap.Push(&pq, &Elem{
+		value:    origin,
+		priority: 0,
+	})
+
+	// seaech
+	for pq.Len() > 0 {
+		v := heap.Pop(&pq).(*Elem)
+		from := v.value.(int)
+		done[from] = true
+		for _, edge := range g.Edges[from] {
+			to := edge.to
+			if done[to] {
+				continue
+			}
+			// 隣接頂点の最小コストを更新
+			nextCost := v.priority + edge.cost
+			if nextCost < g.Cost[to] {
+				g.Cost[to] = nextCost
+				g.Pred[to] = from
+				heap.Push(&pq, &Elem{
+					value:    to,
+					priority: nextCost,
+				})
+			}
+		}
+	}
+}
+
+// toへ向かう最短パスを返す
+func (g *Graph) MinPath(to int) []int {
+	path := make([]int, 0)
+	// 終点から探索
+	path = append(path, to)
+	for node := to; g.Pred[node] != -1; {
+		node = g.Pred[node]
+		path = append(path, node)
+	}
+	// 配列を反転
+	for i, n := 0, len(path); i < n/2; i++ {
+		path[i], path[n-1-i] = path[n-1-i], path[i]
+	}
+	return path
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	first, last := next(), next()
+	n := nextInt()
+	ss := make([]string, 2)
+	ss[0], ss[1] = first, last
+	for i := 0; i < n; i++ {
+		s := next()
+		if s == first || s == last {
+			continue
+		}
+		ss = append(ss, s)
+	}
+
+	if first == last {
+		puts(0)
+		puts(first)
+		puts(last)
+		return
+	}
+
+	m := len(ss)
+	g := NewGraph(m)
+	for i := 0; i < m; i++ {
+		for j := i + 1; j < m; j++ {
+			if diff(ss[i], ss[j]) == 1 {
+				g.AddEdge(i, j, 1)
+				g.AddEdge(j, i, 1)
+			}
+		}
+	}
+
+	g.DijkstraSearch(0)
+
+	if g.Cost[1] == 1<<60 {
+		puts(-1)
+		return
+	}
+
+	puts(g.Cost[1] - 1)
+	path := g.MinPath(1)
+	for _, p := range path {
+		puts(ss[p])
+	}
+}
