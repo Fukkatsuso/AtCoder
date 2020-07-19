e79a8f0c52f3d8a55b60bb75daa66659ebf020ce
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-07-19 23:01:25 +0900

    精進20200719

---
+++ PastProblems/DiffLightBlue/ARC022B.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := nextInt()
+	a := nextInts(n)
+
+	ans := 1
+	mp := map[int]int{}
+	for l, r := 0, 0; l < n; l++ {
+		for r < n && mp[a[r]] == 0 {
+			mp[a[r]]++
+			r++
+		}
+		ans = max(ans, r-l)
+		if l == r {
+			r++
+		}
+		mp[a[l]]--
+	}
+
+	puts(ans)
+}
