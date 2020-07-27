46e5a01542c508f310901b7d965de7e8a69871e7
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-07-27 21:47:05 +0900

    精進20200727

---
+++ PastProblems/DiffGray/m-solutions2020C.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func YesOrNo(b bool) {
+	if b {
+		puts("Yes")
+	} else {
+		puts("No")
+	}
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, k := nextInt(), nextInt()
+	a := nextInts(n)
+
+	for i := k; i < n; i++ {
+		YesOrNo(a[i] > a[i-k])
+	}
+}
