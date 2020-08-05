765ffe7bfa2645f9205c2934f13fb9e9fd930126
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-08-05 22:25:27 +0900

    精進20200805

---
+++ PastProblems/DiffGreen/ABC174E.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func divFloor(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, k := nextInt(), nextInt()
+	a := nextInts(n)
+
+	l, r := 0, max(a...)
+	for l < r-1 {
+		mid := (l + r) / 2
+		x := 0
+		for i := 0; i < n; i++ {
+			x += divFloor(a[i], mid) - 1
+		}
+		if x <= k {
+			r = mid
+		} else {
+			l = mid
+		}
+	}
+
+	puts(r)
+}
