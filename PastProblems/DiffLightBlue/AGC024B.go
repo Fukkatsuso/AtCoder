6c3f57a7b11de3eaed47ae098ae1cf0ade082dd4
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-08-19 22:57:45 +0900

    精進20200819

---
+++ PastProblems/DiffLightBlue/AGC024B.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInt2() (int, int) {
+	return nextInt(), nextInt()
+}
+
+func nextInt3() (int, int, int) {
+	return nextInt(), nextInt(), nextInt()
+}
+
+func nextInt4() (int, int, int, int) {
+	return nextInt(), nextInt(), nextInt(), nextInt()
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := nextInt()
+	p, idx := make([]int, n), make([]int, n)
+	for i := range p {
+		p[i] = nextInt() - 1
+		idx[p[i]] = i
+	}
+
+	// 公差1の単調増加部分列の最長の長さを求める
+	len, maxLen := 1, 1
+	for i := 1; i < n; i++ {
+		if idx[i-1] < idx[i] {
+			len++
+		} else {
+			maxLen = max(maxLen, len)
+			len = 1
+		}
+	}
+
+	ans := n - max(maxLen, len)
+	puts(ans)
+}
