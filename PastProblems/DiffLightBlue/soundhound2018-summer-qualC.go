1f190051d9d355310ce0f498c21211e45d5c3520
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-09-04 23:30:57 +0900

    精進20200904

---
+++ PastProblems/DiffLightBlue/soundhound2018-summer-qualC.go
@@ -0,0 +1,50 @@
+// 解説AC
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, m, d := nextInt(), nextInt(), nextInt()
+
+	var ans float64
+	if d == 0 {
+		ans = float64(m-1) / float64(n)
+	} else {
+		ans = float64(m-1) * 2.0 * float64(n-d) / float64(n*n)
+	}
+	puts(ans)
+}
