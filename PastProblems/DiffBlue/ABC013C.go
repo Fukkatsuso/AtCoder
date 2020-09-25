db3d1a39c0898a7d0720530aef1c306d8adf7af1
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-09-25 21:45:31 +0900

    精進20200925

---
+++ PastProblems/DiffBlue/ABC013C.go
@@ -0,0 +1,82 @@
+// 解説AC
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInt2() (int, int) {
+	return nextInt(), nextInt()
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func min(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v < ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, h := nextInt2()
+	a, b := nextInt2()
+	c, d := nextInt2()
+	e := nextInt()
+
+	ans := a * n
+	for i := 0; i <= n; i++ {
+		rem := (n-i)*e - h - b*i
+		if rem <= 0 {
+			ans = min(ans, i*a)
+			continue
+		}
+		j := rem/(d+e) + 1
+		ans = min(ans, i*a+j*c)
+	}
+
+	puts(ans)
+}
