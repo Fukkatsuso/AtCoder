6fabe7041c59a62c01a11bc75b8f4e894dc8af94
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-10-02 22:21:47 +0900

    精進20201002

---
+++ PastProblems/DiffBlue/ABC021D.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+	mod            = 1000000007
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInt2() (int, int) {
+	return nextInt(), nextInt()
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+const COMMAX int = 1000000
+const MOD int = mod
+
+var fac [COMMAX]int
+var finv [COMMAX]int
+var inv [COMMAX]int
+
+// テーブル作成
+func COMinit() {
+	fac[0] = 1
+	fac[1] = 1
+	finv[0] = 1
+	finv[1] = 1
+	inv[1] = 1
+	for i := 2; i < COMMAX; i++ {
+		fac[i] = fac[i-1] * i % MOD
+		inv[i] = MOD - inv[MOD%i]*(MOD/i)%MOD
+		finv[i] = finv[i-1] * inv[i] % MOD
+	}
+}
+
+// 二項係数nCk
+func COM(n, k int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	COMinit()
+
+	n, k := nextInt2()
+
+	ans := COM(n+k-1, k)
+	puts(ans)
+}
