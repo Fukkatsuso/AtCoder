a2551e50ac1961793587e683315f480911a37fd9
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-10-26 00:31:39 +0900

    精進20201026

---
+++ PastProblems/DiffBrown/ARC106B.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func nextInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = nextInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+type UnionFind struct {
+	Par []int
+}
+
+func NewUnionFind(n int) *UnionFind {
+	uf := &UnionFind{
+		Par: make([]int, n),
+	}
+	uf.Init(n)
+	return uf
+}
+
+func (uf *UnionFind) Init(n int) {
+	for i := 0; i < n; i++ {
+		uf.Par[i] = -1
+	}
+}
+
+func (uf *UnionFind) Find(x int) int {
+	if uf.Par[x] < 0 {
+		return x
+	}
+	uf.Par[x] = uf.Find(uf.Par[x])
+	return uf.Par[x]
+}
+
+func (uf *UnionFind) Unite(x, y int) {
+	x, y = uf.Find(x), uf.Find(y)
+	if x == y {
+		return
+	}
+
+	if uf.Par[x] > uf.Par[y] {
+		x, y = y, x
+	}
+	uf.Par[x] += uf.Par[y]
+	uf.Par[y] = x
+}
+
+func (uf *UnionFind) Same(x, y int) bool {
+	return uf.Find(x) == uf.Find(y)
+}
+
+// xの属する集合の要素数
+func (uf *UnionFind) Size(x int) int {
+	return -uf.Par[uf.Find(x)]
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, m := nextInt(), nextInt()
+	a, b := nextInts(n), nextInts(n)
+
+	uf := NewUnionFind(n)
+	for i := 0; i < m; i++ {
+		c, d := nextInt()-1, nextInt()-1
+		uf.Unite(c, d)
+	}
+
+	sum := map[int]int{}
+	for i := 0; i < n; i++ {
+		root := uf.Find(i)
+		sum[root] += a[i] - b[i]
+	}
+	for _, v := range sum {
+		if v != 0 {
+			puts("No")
+			return
+		}
+	}
+	puts("Yes")
+}
