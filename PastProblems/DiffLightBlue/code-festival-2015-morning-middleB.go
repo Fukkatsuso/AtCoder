93bee5da2a5bf4a9f1f2f9a59be3272db3742d02
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-10-31 22:36:57 +0900

    精進20201031

---
+++ PastProblems/DiffLightBlue/code-festival-2015-morning-middleB.go
@@ -0,0 +1,86 @@
+// 解説AC
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func min(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v < ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func LCS(s, t string) int {
+	n, m := len(s), len(t)
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, m+1)
+	}
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			x := dp[i-1][j-1]
+			if s[i-1] == t[j-1] {
+				x++
+			}
+			dp[i][j] = max(dp[i-1][j], dp[i][j-1], x)
+		}
+	}
+	return dp[n][m]
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, s := nextInt(), next()
+
+	ans := n
+	for i := 1; i < n; i++ {
+		ans = min(ans, n-2*LCS(s[:i], s[i:]))
+	}
+	puts(ans)
+}
