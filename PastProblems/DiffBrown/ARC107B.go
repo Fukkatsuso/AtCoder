2169625d66d5349e037938c23bdeb4897c2f1f82
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-11-11 01:12:19 +0900

    精進20201111

---
+++ PastProblems/DiffBrown/ARC107B.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func min(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v < ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func f(x, n int) int {
+	return min(x-1, n) - max(x-n, 1) + 1
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, k := getInt(), getInt()
+
+	ans := 0
+	for x := 2; x <= n*2; x++ {
+		// x=a+b, y=c+d
+		y := x - k
+		if y < 2 || n*2 < y {
+			continue
+		}
+		ans += f(x, n) * f(y, n)
+	}
+	puts(ans)
+}
