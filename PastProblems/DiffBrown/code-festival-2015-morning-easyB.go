18b3a01712b31f113b15f50e8be688ec6c9c0770
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-12-07 21:01:31 +0900

    精進20201207

---
+++ PastProblems/DiffBrown/code-festival-2015-morning-easyB.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+	s := gets()
+
+	if n%2 == 1 {
+		puts(-1)
+		return
+	}
+
+	ans := 0
+	for i := 0; i < n/2; i++ {
+		if s[i] != s[i+n/2] {
+			ans++
+		}
+	}
+
+	puts(ans)
+}
