33ea281fcb21e77a487df50ccb471102a58867ed
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-12-16 20:24:10 +0900

    精進20201216

---
+++ PastProblems/DiffBlue/ABC083D.go
@@ -0,0 +1,76 @@
+// 解説AC
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func min(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v < ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	s := gets()
+	n := len(s)
+
+	k := n
+	for i := 0; i < n-1; i++ {
+		if s[i] != s[i+1] {
+			k = min(k, max(i+1, n-i-1))
+		}
+	}
+
+	if k == 0 {
+		puts(n)
+	} else {
+		puts(k)
+	}
+}
