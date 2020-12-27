d36277e0446aec482f5170daf14096b63b083629
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-12-27 23:50:35 +0900

    精進20201227

---
+++ PastProblems/DiffGray/ARC002A.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	y := getInt()
+
+	ok := (y%400 == 0) || (y%100 != 0 && y%4 == 0)
+
+	if ok {
+		puts("YES")
+	} else {
+		puts("NO")
+	}
+}
