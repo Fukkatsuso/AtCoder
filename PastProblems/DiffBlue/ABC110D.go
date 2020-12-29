9eab619aab77c81d967a544cddb5633a65fd33d3
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-12-29 17:19:01 +0900

    精進20201229

---
+++ PastProblems/DiffBlue/ABC110D.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+	mod            = 1000000007
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func primeFactor(n int) map[int]int {
+	m := map[int]int{}
+	for i := 2; i*i <= n; i++ {
+		for n%i == 0 {
+			m[i]++
+			n /= i
+		}
+	}
+	if n != 1 {
+		m[n] = 1
+	}
+	return m
+}
+
+const COMMAX int = 1000000
+const MOD int = mod
+
+var fac [COMMAX]int
+var finv [COMMAX]int
+var inv [COMMAX]int
+
+// テーブル作成
+func init() {
+	fac[0] = 1
+	fac[1] = 1
+	finv[0] = 1
+	finv[1] = 1
+	inv[1] = 1
+	for i := 2; i < COMMAX; i++ {
+		fac[i] = fac[i-1] * i % MOD
+		inv[i] = MOD - inv[MOD%i]*(MOD/i)%MOD
+		finv[i] = finv[i-1] * inv[i] % MOD
+	}
+}
+
+// 二項係数nCk
+func COM(n, k int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, m := getInt(), getInt()
+
+	pf := primeFactor(m)
+	ans := 1
+	for _, v := range pf {
+		ans *= COM(v+n-1, v)
+		ans %= mod
+	}
+
+	puts(ans)
+}
