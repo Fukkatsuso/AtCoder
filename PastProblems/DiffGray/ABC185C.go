5ac7643718268a32f5cf8f4d536efd5aae91a868
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-01-14 21:43:04 +0900

    精進20210114

---
+++ PastProblems/DiffGray/ABC185C.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	dp[0][0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 0; j <= i; j++ {
+			if j >= 1 {
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	puts(dp[n-1][11])
+}
