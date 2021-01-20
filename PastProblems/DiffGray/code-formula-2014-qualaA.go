9e21e7426195f842b87f233b9140a9383689a03f
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-01-20 22:29:23 +0900

    精進20210120

---
+++ PastProblems/DiffGray/code-formula-2014-qualaA.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	a := getInt()
+
+	for i := 1; i <= 100; i++ {
+		if i*i*i == a {
+			puts("YES")
+			return
+		}
+	}
+
+	puts("NO")
+}
