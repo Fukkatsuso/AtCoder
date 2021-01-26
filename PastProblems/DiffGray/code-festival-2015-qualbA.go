127ee0628c0dd320c32ea04ee6e634f4fb265e0b
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-01-26 21:56:34 +0900

    精進20210126

---
+++ PastProblems/DiffGray/code-festival-2015-qualbA.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	s := gets()
+	puts(s + s)
+}
