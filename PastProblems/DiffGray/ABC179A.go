ba7c7afa0e8a1fb63ad3f880bd5279197b4c81e1
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-02-01 23:10:50 +0900

    精進20210201

---
+++ PastProblems/DiffGray/ABC179A.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func putf(format string, a ...interface{}) {
+	fmt.Fprintf(wt, format, a...)
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	s := gets()
+
+	if s[len(s)-1] == 's' {
+		putf("%ses\n", s)
+	} else {
+		putf("%ss\n", s)
+	}
+}
