40dde2963badcc1d88555c18dc7dd9b78cc9f972
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-02-08 21:11:44 +0900

    精進20210208

---
+++ PastProblems/DiffGray/tokiomarine2020B.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	a, v := getInt(), getInt()
+	b, w := getInt(), getInt()
+	t := getInt()
+
+	if abs(b-a)+w*t <= v*t {
+		puts("YES")
+	} else {
+		puts("NO")
+	}
+}
