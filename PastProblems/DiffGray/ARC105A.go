813dcacbc3720547f71a9dde44ac16dd4bd7a7f6
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-02-10 20:09:41 +0900

    精進20210210

---
+++ PastProblems/DiffGray/ARC105A.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func getInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = getInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func sum(a ...int) int {
+	ret := 0
+	for _, v := range a {
+		ret += v
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	c := getInts(4)
+	s := sum(c...)
+
+	for i := 0; i < 3; i++ {
+		if 2*c[i] == s {
+			puts("Yes")
+			return
+		}
+		for j := i + 1; j < 4; j++ {
+			if 2*(c[i]+c[j]) == s {
+				puts("Yes")
+				return
+			}
+		}
+	}
+	puts("No")
+}
