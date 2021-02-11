86e441fd7b41f9c00ed2a0e9f9df0d5c38bddeb5
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-02-11 22:56:12 +0900

    精進20210211

---
+++ PastProblems/DiffGray/ABC180B.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func getInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = getInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func sum(a ...int) int {
+	ret := 0
+	for _, v := range a {
+		ret += v
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+	x := make([]int, n)
+	for i := range x {
+		x[i] = abs(getInt())
+	}
+
+	m := sum(x...)
+	puts(m)
+
+	e := 0
+	for i := range x {
+		e += x[i] * x[i]
+	}
+	puts(math.Sqrt(float64(e)))
+
+	c := max(x...)
+	puts(c)
+}
