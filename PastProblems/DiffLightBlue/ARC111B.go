30faa60bc279cc9ffed4f3abefc610360ba9ba9a
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-02-19 23:33:55 +0900

    精進20210219

---
+++ PastProblems/DiffLightBlue/ARC111B.go
@@ -0,0 +1,123 @@
+// 解説AC
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+	MAXN           = 400000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+type UnionFind struct {
+	Par  []int
+	Loop []bool
+}
+
+func NewUnionFind(n int) *UnionFind {
+	uf := &UnionFind{
+		Par:  make([]int, n),
+		Loop: make([]bool, n),
+	}
+	uf.Init(n)
+	return uf
+}
+
+func (uf *UnionFind) Init(n int) {
+	for i := 0; i < n; i++ {
+		uf.Par[i] = -1
+	}
+}
+
+func (uf *UnionFind) Find(x int) int {
+	if uf.Par[x] < 0 {
+		return x
+	}
+	uf.Par[x] = uf.Find(uf.Par[x])
+	return uf.Par[x]
+}
+
+func (uf *UnionFind) Unite(x, y int) {
+	x, y = uf.Find(x), uf.Find(y)
+	if x == y {
+		uf.Loop[x] = true
+		return
+	}
+
+	if uf.Par[x] > uf.Par[y] {
+		x, y = y, x
+	}
+	uf.Par[x] += uf.Par[y]
+	uf.Par[y] = x
+	uf.Loop[x] = uf.Loop[x] || uf.Loop[y]
+}
+
+func (uf *UnionFind) Same(x, y int) bool {
+	return uf.Find(x) == uf.Find(y)
+}
+
+// xの属する集合の要素数
+func (uf *UnionFind) Size(x int) int {
+	return -uf.Par[uf.Find(x)]
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+	uf := NewUnionFind(MAXN)
+	for i := 0; i < n; i++ {
+		a, b := getInt()-1, getInt()-1
+		uf.Unite(a, b)
+	}
+
+	ans := 0
+	for i := 0; i < MAXN; i++ {
+		if i != uf.Find(i) {
+			continue
+		}
+		if !uf.Loop[i] {
+			ans += uf.Size(i) - 1
+		} else {
+			ans += uf.Size(i)
+		}
+	}
+	puts(ans)
+}
