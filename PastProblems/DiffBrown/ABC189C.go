4973f466765ac36a97aec1e0c75896859ad4055b
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-02-24 21:52:32 +0900

    精進20210224

---
+++ PastProblems/DiffBrown/ABC189C.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func getInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = getInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func min(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v < ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v > ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+	a := getInts(n)
+
+	ans := 0
+	for l := 0; l < n; l++ {
+		x := 1 << 60
+		for r := l; r < n; r++ {
+			x = min(x, a[r])
+			ans = max(ans, (r-l+1)*x)
+		}
+	}
+
+	puts(ans)
+}
