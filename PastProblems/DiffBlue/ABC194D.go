f4bf931b5c1f73ba5e69fefcbea5c252110649be
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-03-07 00:51:08 +0900

    精進20210307

---
+++ PastProblems/DiffBlue/ABC194D.go
@@ -0,0 +1,49 @@
+// 解説AC
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+
+	ans := 0.0
+	for i := 1; i < n; i++ {
+		ans += float64(n) / float64(n-i)
+	}
+
+	puts(ans)
+}
