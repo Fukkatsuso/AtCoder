2b163d6fc507fabeda59515eb10e901881e7e1c3
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-03-23 23:47:15 +0900

    精進20210323

---
+++ PastProblems/DiffGray/hhkb2020C.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func getInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = getInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+
+	ans := 0
+	p := map[int]bool{}
+	for i := 0; i < n; i++ {
+		pi := getInt()
+		p[pi] = true
+		for ; p[ans]; ans++ {
+		}
+		puts(ans)
+	}
+}
