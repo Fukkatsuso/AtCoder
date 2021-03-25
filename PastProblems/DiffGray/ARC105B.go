1e0926c8af4bea0900f7062e141c7bd7f4db7061
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-03-25 20:44:59 +0900

    精進20210325

---
+++ PastProblems/DiffGray/ARC105B.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func getInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = getInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	for a%b != 0 {
+		a, b = b, a%b
+	}
+	return b
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+	a := getInts(n)
+
+	ans := a[0]
+	for i := 1; i < n; i++ {
+		ans = gcd(ans, a[i])
+	}
+	puts(ans)
+}
